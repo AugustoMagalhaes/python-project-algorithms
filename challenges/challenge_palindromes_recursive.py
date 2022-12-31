0cc64e9e73bf8d6f997818d8f65f8372a4c81a05
AugustoMagalhaes <augustommg@gmail.com>
2022-12-31 01:47:46 -0300

    feat: implementa is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word or word[low_index] != word[high_index]:
+        return False
+    if low_index >= high_index:
+        return True
+
+    next_low_index, next_high_index = low_index + 1, high_index - 1
+    return is_palindrome_recursive(word, next_low_index, next_high_index)
