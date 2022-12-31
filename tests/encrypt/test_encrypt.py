b6e8d7f05c1d9a2f147b59be5433ae5439e31995
AugustoMagalhaes <augustommg@gmail.com>
2022-12-31 01:09:36 -0300

    test: implementa test_encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,25 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    """ with pytest.raises(TypeError, match="tipo inválido para key") as _:
+        encrypt_message(1, 1) """
+
+    invalid_key = encrypt_message("ABCD", 20)
+    assert invalid_key == "DCBA"
+
+    odd_key_valid = encrypt_message("ABCD", 3)
+    assert odd_key_valid == "CBA_D"
+
+    even_key_valid = encrypt_message("ABCD", 2)
+    assert even_key_valid == "DC_BA"
+
+    with pytest.raises(TypeError) as e_message:
+        encrypt_message([], 1)
+    assert "tipo inválido para message" in e_message.value.args
+
+    with pytest.raises(TypeError) as e_key:
+        encrypt_message("ABCD", [])
+    assert "tipo inválido para key" in e_key.value.args
