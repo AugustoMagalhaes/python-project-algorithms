f2ccfda6414d5df4dd996cefaa0ff24f6cf7361e
AugustoMagalhaes <augustommg@gmail.com>
2022-12-31 00:43:20 -0300

    feat: implementa study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None or len(permanence_period) == 0:
+        return None
+    try:
+        output = [i for i in permanence_period if i[0] <= target_time <= i[1]]
+    except (TypeError):
+        return None
+
+    number_of_students = len(output)
+
+    return number_of_students
