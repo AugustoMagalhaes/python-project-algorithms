4979331783938511ba916d06743f30b54a758fcd
AugustoMagalhaes <augustommg@gmail.com>
2023-01-03 02:28:08 -0300

    feat: implementa find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,25 @@
+def validate_nums(nums):
+    if not nums or type(nums) not in [list, tuple, set]:
+        return False
+
+    if len(nums) == len(set(nums)) or len(nums) == 0:
+        return False
+
+    return True
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not validate_nums(nums):
+        return False
+
+    sorted_nums = sorted(nums)
+
+    if sorted_nums[0] < 0:
+        return False
+
+    for index, num in enumerate(sorted_nums):
+        next_index = index + 1 if index != len(sorted_nums) - 1 else index
+        if num == sorted_nums[next_index]:
+            return num
+
+    return False
