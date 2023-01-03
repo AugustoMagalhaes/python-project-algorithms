04d3e470814cf572360f6b7a3a594b55281bd161
AugustoMagalhaes <augustommg@gmail.com>
2023-01-03 01:41:55 -0300

    feat: implementa is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,10 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        if not word or type(word) is not str:
+            return False
+        word_as_list = list(word)
+        reversed_word_list = word_as_list[-1::-1]
+
+        return word_as_list == reversed_word_list
+    except TypeError:
+        return None
